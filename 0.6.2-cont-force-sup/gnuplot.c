5ba0c681054b8c90ee8a8b69ee2a9d606792769b
Tae Seung Kang <tsk@cyclone.cise.ufl.edu>
2015-02-23 11:20:00 -0500

    all

--- 0.6.2-cont-force-sup/gnuplot.c
+++ 0.6.2-cont-force-sup/gnuplot.c
@@ -69,7 +69,7 @@ void plot(int col) {
 	if(col == 1) 
         	fprintf(gp, "\"<(sed -n '1,%dp' %s)\" using ($2 * 2) title 'R'\n", sample_size, fname);
 	// last steps
-	//fprintf(gp, "set xtics %d,180000 nomirror\n", lastlines);
+	fprintf(gp, "set xtics %d,%d nomirror\n", lastlines, nsteps * 1000);
 	//fprintf(gp, "set xtics (\"%d\" 1, \"%d\" 100, \"%d\" 200, \"%d\" 300, \"%d\" 400, \"%d\" 500)\n", lastlines, lastlines + 100, 
 	//			lastlines + 200, lastlines + 300, lastlines + 400, nsteps * 1000);
         fprintf(gp, "plot \"<(sed -n '%d,%dp' %s)\" using %d title '%s' %s\n", lastlines, nsteps * 1000, fname, col, colstr, type);
